ab56effbf9ef1ff4b3a8eb9003fa58f06f87499b
atelierog <atelierog.co@gmail.com>
2026-09-22 23:50:58 +0530

    Lock down legacy Super Admin RPCs

---
+++ supabase/migrations/20260922_lock_down_legacy_super_admin_rpcs.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.bootstrap_platform_admin() from authenticated;
+revoke execute on function public.super_admin_update_gym_details(uuid,text,text) from authenticated;
