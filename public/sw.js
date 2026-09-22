8b09c186d1a4d762113489ad9ba5b7c36364de17
atelierog <atelierog.co@gmail.com>
2026-09-22 18:28:37 +0530

    Refresh GymOS service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="gymos-v1";
+const CACHE="gymos-v2";
 const SHELL=["/","/index.html","/manifest.json","/icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
