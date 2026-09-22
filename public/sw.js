aee1f3dc5ddbd245d531988f8624308f34a7708c
atelierog <atelierog.co@gmail.com>
2026-09-22 18:01:37 +0530

    Improve PWA offline caching

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,5 @@
-self.addEventListener("install",()=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+const CACHE="gymos-v1";
+const SHELL=["/","/index.html","/manifest.json","/icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>cached||caches.match("/index.html"))))});
